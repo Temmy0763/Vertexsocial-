cfcf34740e2f63870ae39d949553db2bce8cdf49
Adeleke Temiloluwa C <adeleketemmy0763@gmail.com>
2026-02-19 17:30:12 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import ComingSoon from './ComingSoon';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ComingSoon />
+    </div>
+  );
+}
+
+export default App;
